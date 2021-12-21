9f7ec647f6a7b9548b7f128c20ddbda66752e533
ivymmurithi <ivymurithi@gmail.com>
2021-12-21 15:44:13 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'Converter';
+  public unit: any; 
+  public results: any;
+
+  convert(userInput: any) {
+    this.results = this.unit === "f" ? (parseFloat(userInput) * 1.8) + 32 : ( 273.15 + parseFloat(userInput));
+  }
 }
